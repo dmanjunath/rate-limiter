9fd9cd04a328bf36fe69e63861d21716e46424b3
Dheeraj Manjunath <dheeraj@dheeraj-MXG061.(none)>
2013-07-26 12:26:45 -0400

    multiple objects

--- test/rate_limit_test.js
+++ test/rate_limit_test.js
@@ -4,31 +4,57 @@ var assert = require('assert');
 
 
 var rateLimit = new RateLimiter(5, 2000);
+var rateLimit2 = new RateLimiter(10, 10000);
 var callCount = 0;
+var callCount2 = 0;
+
 var something = function(s, d){
   console.log(s);
   console.log(d)
   console.log(callCount+=1)
 }
 
+var something2 = function(s, d){
+  console.log("THIS IS TWO: " + s);
+  console.log(d)
+  console.log(callCount2+=1)
+}
 
-rateLimit.callWithLimit(something,["dork", "on"])
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
 
-assert.equal(callCount, 5); //should have called five times
+rateLimit2.callWithLimit(something2,["dork", "on"])
+rateLimit2.callWithLimit(something2,["dork"])
+rateLimit2.callWithLimit(something2,["dork"])
+rateLimit2.callWithLimit(something2,["dork"])
+rateLimit2.callWithLimit(something2,["dork"])
 
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
+rateLimit.callWithLimit(something,["one", "on"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
 
-rateLimit.callWithLimit(something,["dork"])
-rateLimit.callWithLimit(something,["dork"])
-setTimeout(function(){
-  assert.equal(callCount, 12); //shoud have called 12 times
 
-},4100)
+
+// assert.equal(callCount, 5); //should have called five times
+
+rateLimit.callWithLimit(something,["one", "on"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+
+rateLimit2.callWithLimit(something2,["dork", "on"])
+rateLimit2.callWithLimit(something2,["dork"])
+rateLimit2.callWithLimit(something2,["dork"])
+rateLimit2.callWithLimit(something2,["dork"])
+rateLimit2.callWithLimit(something2,["dork"])
+
+rateLimit.callWithLimit(something,["one", "on"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+rateLimit.callWithLimit(something,["one"])
+// setTimeout(function(){
+//   assert.equal(callCount, 22); //shoud have called 12 times
+
+// },4100)
